5c8a7119ee55498c55ec48efd2390ec24d0796af
Ben Whipp <ben@benwhipp.dev>
2024-10-31 23:27:00 +0000

    Add mobile styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,25 +11,27 @@ const Home = () => {
   const [resetAlive, setResetAlive] = useState(false);
 
   return (
-    <div className="size-screen flex items-center justify-center gap-8 bg-gray-800">
-      <div className="flex flex-col gap-4">
+    <div className="box-border flex h-screen w-screen flex-col-reverse items-center justify-center gap-8 bg-gray-800 p-4 sm:flex-row">
+      <div className="flex flex-col-reverse gap-4 sm:flex-col">
         <h1>Conway&apos;s Game of Life</h1>
-        <Button onClick={() => setPlaying(!playing)}>
-          {playing ? "Stop" : "Start"}
-        </Button>
-        <div className="flex gap-4">
-          <Button onClick={() => resetGrid(resetAlive)}>Reset</Button>
-          <div className="flex w-full flex-col items-center gap-2">
-            <Cell
-              alive={resetAlive}
-              updateCell={() => {
-                setResetAlive(!resetAlive);
-              }}
-            />
-            <label>Set all {}</label>
+        <div className="flex flex-col gap-4">
+          <Button onClick={() => setPlaying(!playing)}>
+            {playing ? "Stop" : "Start"}
+          </Button>
+          <div className="flex gap-4">
+            <Button onClick={() => resetGrid(resetAlive)}>Reset</Button>
+            <div className="flex w-full flex-col items-center gap-2">
+              <Cell
+                alive={resetAlive}
+                updateCell={() => {
+                  setResetAlive(!resetAlive);
+                }}
+              />
+              <label>Set all {}</label>
+            </div>
           </div>
+          <Button onClick={randomizeGrid}>Randomise</Button>
         </div>
-        <Button onClick={randomizeGrid}>Randomise</Button>
       </div>
       <Grid grid={grid} updateCell={updateCell} />
     </div>
--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -10,7 +10,7 @@ interface GridProps {
 
 const GridComponent = ({ grid, updateCell }: GridProps) => {
   return (
-    <div className="grid h-[100vmin] w-[100vmin] grid-cols-[repeat(64,minmax(0,1fr))] border-2 border-gray-500">
+    <div className="grid aspect-square h-full max-h-full w-full max-w-full grid-cols-[repeat(64,minmax(0,1fr))] grid-rows-[repeat(64,minmax(0,1fr))] border-2 border-gray-500 p-1">
       {grid.map((row, i) => (
         <Fragment key={`row-${i}`}>
           {row.map((cell: boolean, j: number) => (
